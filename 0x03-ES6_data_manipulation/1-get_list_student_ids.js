165949a82a8f94ac65e4a8d5d17ed0577adc0630
EHronek <emmanuelhronek@gmail.com>
2025-04-22 19:31:54 +0300

    error fix

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,6 @@
 export default function getListStudentIds(list) {
   if (!Array.isArray(list)) {
     return [];
-  } else {
-    return list.map((item) => item.id);
   }
+  return list.map((item) => item.id);
 }
