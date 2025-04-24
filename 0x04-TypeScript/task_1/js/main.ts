f23fbee3e8a53af5eb8ac49d64a50242abd245c5
EHronek <emmanuelhronek@gmail.com>
2025-04-24 16:26:00 +0300

    Extends interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,4 +1,4 @@
-interface Teacher {
+export interface Teacher {
     readonly firstName: string;
     readonly lastName: string;
     fullTimeEmployee: boolean;
@@ -6,6 +6,19 @@ interface Teacher {
     location: string;
     [key: string]: any;
 }
+
+export interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+console.log(director1);
 
 const teacher3: Teacher = {
     firstName: 'John',
