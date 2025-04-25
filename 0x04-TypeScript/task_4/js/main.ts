c9480a129fc0f515c51a4fdd9b6f422575bf7626
EHronek <emmanuelhronek@gmail.com>
2025-04-25 21:47:59 +0300

    namespace

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,33 +1,42 @@
 /// <reference path="./subjects/Teacher.ts" />
 /// <reference path="./subjects/Subject.ts" />
 /// <reference path="./subjects/Cpp.ts" />
-/// <reference path="./subjects/React.ts" />
 /// <reference path="./subjects/Java.ts" />
+/// <reference path="./subjects/React.ts" />
 
 namespace Subjects {
-    const cpp = new Cpp();
-    const react = new React();
-    const java = new Java();
+    // Export constants as required
+    export const cpp = new Cpp();
+    export const java = new Java();
+    export const react = new React();
 
-    const cTeacher: Teacher = {
+    export const cTeacher: Teacher = {
         firstName: "John",
         lastName: "Doe",
         experienceTeachingC: 10
     };
 
-    // Cpp Subject
-    cpp.setTeacher(cTeacher);
+    // For Cpp Subject
     console.log("C++");
+    cpp.setTeacher(cTeacher);
     console.log(cpp.getRequirements());
     console.log(cpp.getAvailableTeacher());
 
-    // Java Subject
+    // For Java Subject
     console.log("Java");
+    java.setTeacher(cTeacher);
     console.log(java.getRequirements());
     console.log(java.getAvailableTeacher());
 
-    // React Subject
+    // For React Subject
     console.log("React");
+    react.setTeacher(cTeacher);
     console.log(react.getRequirements());
     console.log(react.getAvailableTeacher());
 }
+
+// Export the constants at the top level
+export const cpp = Subjects.cpp;
+export const java = Subjects.java;
+export const react = Subjects.react;
+export const cTeacher = Subjects.cTeacher;
