73f07ff397a5b8ef1908790521d49261f9c2c11c
EHronek <emmanuelhronek@gmail.com>
2025-06-10 22:43:19 +0300

    http server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,18 +1,20 @@
 const http = require('http');
 
-const HOST = 'localhost';
 const PORT = 1245;
+const HOST = 'localhost';
 const app = http.createServer();
 
 app.on('request', (_, res) => {
-    const responseTxt = 'Hello ALX!';
+  const responseText = 'Hello Holberton School!';
 
-    res.setHeader('Content-Type', 'text/plain');
-    res.setHeader('Content-Length', responseTxt.length);
-    res.statusCode = 200;
-    res.write(Buffer.from(responseTxt));
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
 });
+
 app.listen(PORT, HOST, () => {
-    process.stdout.write(`Server listening at Port ${PORT}\n`);
+  process.stdout.write(`Server listening at -> http://${HOST}:${PORT}\n`);
 });
+
 module.exports = app;
