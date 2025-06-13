0a0128c820260420f7b4d8ef894af6bab978401c
EHronek <emmanuelhronek@gmail.com>
2025-06-13 20:57:19 +0300

    Organize a complex HTTP server using Express

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -16,18 +16,18 @@ const readDatabase = (dataPath) => new Promise((resolve, reject) => {
           .split('\n');
         const studentGroups = {};
         const dbFieldNames = fileLines[0].split(',');
-        const studentPropNames = dbFieldNames
+        const studentPropertyNames = dbFieldNames
           .slice(0, dbFieldNames.length - 1);
 
         for (const line of fileLines.slice(1)) {
-          const studentRecord = line.split(',');
-          const studentPropValues = studentRecord
-            .slice(0, studentRecord.length - 1);
-          const field = studentRecord[studentRecord.length - 1];
+          const studRecord = line.split(',');
+          const studentPropValues = studRecord
+            .slice(0, studRecord.length - 1);
+          const field = studRecord[studRecord.length - 1];
           if (!Object.keys(studentGroups).includes(field)) {
             studentGroups[field] = [];
           }
-          const studentEntries = studentPropNames
+          const studentEntries = studentPropertyNames
             .map((propName, idx) => [propName, studentPropValues[idx]]);
           studentGroups[field].push(Object.fromEntries(studentEntries));
         }
